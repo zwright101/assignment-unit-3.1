e1c8cdc35e600a31f3c4d76c4036821cd1718c29
mbmosman <mbMosman@users.noreply.github.com>
2021-03-05 18:00:16 -0600

    initial split

---
+++ README.md
@@ -0,0 +1,37 @@
+# Prime Tier 1 Week 3 - Part 1
+
+This week, we will continue to put more into your tool belt by tackling methods for handling tasks multiple times, storing multiple pieces of information, and more advanced decision tools.
+
+## Topics Covered
+
+* Loops `for`, `while`, `for of`
+* Arrays
+
+## Assignment
+
+As always, start off by forking and cloning this repository from GitHub. Open the code up in Atom to get started. 
+
+> Note that in the future we will not call this out explicitly. We'll assume you already know that is how you start your work. 
+
+### Reminder - Levels of Difficulty
+
+- Required Features are required and focus on the key skills for the week
+- Stretch Goals are __*optional*__!
+  - These will stretch and challenge deeper understanding of the material.
+  - Note that these may require additional research beyond the provided content. 
+
+### Files Provided
+
+- *index.html* (Note: array-practice.js is already sourced in)
+- *array-practice.js* (Required Features, & Stretch Goals)
+
+In the file there are some examples of working with arrays followed by __TODO__ comments with tasks for you to complete by writing some code following up on what is shown in the example. 
+
+> Please do not remove the instruction comments as you complete your work. 
+
+
+## Assignment Submission
+Check in your repo, then turn in your work via the Prime Academy Assignment Application at http://primeacademy.io, as usual and don't hesitate to hit up the Slack channel as needed!
+
+**REMINDER:** Make sure to answer the Slack discussion question for this week!
+
---
+++ assignment/scripts/array-practice.js
@@ -0,0 +1,77 @@
+console.log('****** Array Practice *******');
+
+// 1. Creating arrays
+console.log('--- 1. Practice creating arrays ---');
+
+// Example: Array of animals
+let animalArray = ['fish', 'cat', 'bird', 'dog'];
+console.log('Animals are: ', animalArray);
+
+// 1.a. TODO: Make an array with some favorite foods
+
+
+// 1.b. TODO: Log your array of foods to the console with a message, similar 
+//      to the example above
+
+
+
+// 2. Array.length - tells you how many items are in the array
+console.log('--- 2. Length of an array ---');
+
+// Example: How many animals are in the array? 
+console.log('Number of animals:', animalArray.length);
+
+// 2.a. TODO: Log to the console the number of foods in your array
+
+
+// 3. Accessing array items
+console.log('--- 3. Accessing items in an array ---');
+
+// Example: Log the first animal from the array using it's array index
+console.log('First animal is', animalArray[0])
+
+// 3.a. TODO: Log the second animal in the array 
+
+
+// 3.b. TODO: Log the last animal in the array using it's array index 
+
+
+// 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
+//      instead of the exact index number of the last item
+
+
+
+// 4. Adding & Removing Array Items
+console.log('--- 4. Adding and removing array items ---');
+
+// Example: Add an animal to the end using Array.push
+animalArray.push('penguin');
+console.log('Added an animal to end,', animalArray);
+
+// 4.a. TODO: Add a new food at the end of your array & log the array
+
+
+
+// Example: Remove the last animal by using Array.pop
+let removedAnimal = animalArray.pop();
+console.log('Removed the last animal', removedAnimal);
+console.log('The animals are now', animalArray);
+
+// 4.b. TODO: Remove the food at the end of your array & 
+//      log both the food removed and the updated array
+
+
+// Example: Add an animal to the beginning using Array.unshift
+animalArray.unshift('walrus');
+console.log(`Added an animal to beginning: ${animalArray}`);
+
+// 4.c. TODO: Add a food at the beginning of the array & log the array
+
+
+// Example: Remove the first animal using Array.shift
+removedAnimal = animalArray.shift();
+console.log('Removed the first animal', removedAnimal);
+console.log('The animals are now', animalArray);
+
+// 4.d TODO: Remove the food at the beginning of your array & 
+//     log both the food removed and the updated array
